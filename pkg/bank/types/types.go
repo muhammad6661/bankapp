0139d3a131da0f58b2feafd0616467d013c33c78
muhammad6661 <nurmatov.muhammad000@gmail.com>
2021-03-25 20:57:15 +0500

    Add type file

---
+++ pkg/bank/types/types.go
@@ -0,0 +1,26 @@
+package types
+
+type Money int64
+type Currency string
+const(
+	TJS Currency ="TJS"
+	RUB Currency ="RUB"
+	USD Currency ="USD"
+
+)
+type PAN string
+type Card struct{
+	ID int 
+	PAN PAN
+	Balance Money
+	Currency Currency
+	Color string
+	Name string
+	Active bool
+}
+
+//Payment for payment information
+type Payment struct {
+	ID int 
+	Amount Money
+}
